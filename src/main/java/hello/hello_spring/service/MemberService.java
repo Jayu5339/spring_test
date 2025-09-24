fb635873a5431c838a3b87b05d1e4837f0e3c313
Jayu5339 <yutvja6@gmail.com>
2025-09-24 13:48:13 +0900

    [Feat ✨](Add JpaMemberRepository implementation): Implement MemberRepository with JPA

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -26,6 +26,7 @@ public class MemberService {
         validateDuplicateMember(member); // 중복 회원 검증
         memberRepository.save(member);
         return member.getId();
+
     }
 
     private void validateDuplicateMember(Member member) {
@@ -36,7 +37,9 @@ public class MemberService {
     }
 
     public List<Member> findMembers() {
+
         return memberRepository.findAll();
+
     }
 
     public Optional<Member> findOne(Long memberID) {
